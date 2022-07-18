dea2f678e09b509e94709468f73cdcc77b3f5d48
Vladislav-Nickolaevich <97509591+Vladislav-Nickolaevich@users.noreply.github.com>
2022-07-18 19:11:11 +0300

    Fix project

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-/* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
@@ -25,33 +13,6 @@ const movieDB = {
 };
 
 
-// const adv = document.querySelectorAll('.promo__adv img'),
-//       genre = document.querySelector('.promo__genre'),
-//       bg = document.querySelector('.promo__bg'),
-//       listFilm = document.querySelector('.promo__interactive-list');
-
-// adv.forEach(item => {
-//     item.remove();
-// });
-// genre.textContent = 'драма';
-// bg.style.backgroundImage = 'url("img/bg.jpg")';
-// listFilm.innerHTML = '';
-// movieDB.movies.sort();
-// movieDB.movies.forEach((item, i) => {
-//     listFilm.innerHTML += `
-//     <li class="promo__interactive-item">${i + 1} ${item}
-//     <div class="delete"></div>
-//     </li>   
-//     `;
-// });
-
-
-
-
-
-
-
-// Практика 2
 const adv = document.querySelectorAll('.promo__adv img'),
       genre = document.querySelector('.promo__genre'),
       bg = document.querySelector('.promo__bg'),
